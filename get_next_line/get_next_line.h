cbfb5fd539bbc71db5ea2a95526396b66de51522
eabradol <eabradol@student.21-school.ru>
2022-03-21 14:47:35 +0300

    1

---
+++ get_next_line/get_next_line.h
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By:  <>                                        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/10/18 13:19:01 by                   #+#    #+#             */
+/*   Updated: 2021/10/22 09:46:33 by                  ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include	<stdio.h>
+# include	<unistd.h>
+# include	<stdlib.h>
+# include <fcntl.h>
+
+char	*ft_strdup(const char *s1);
+char	*ft_strjoin(char *s1, char *s2);
+char	*ft_strchr(const char *s, int c);
+size_t	ft_strlen(const char *str);
+char	*ft_substr(char const *s, unsigned int start, size_t len);
+char	*get_next_line(int fd);
+
+#endif
